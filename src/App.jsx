9b5bf4b2e776b3dbf652267c349a8fb595f82c9f
Nico2-NT <nicolastraver93@gmail.com>
2025-05-31 23:39:17 +0100

    Improved the nav bar, removed the search bar from navbar to below the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import {
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import PublishRide from "./Components/PublishRide";
+import SearchBar from "./Components/SearchBar";
+import "./Components/Styles/Navbar.css";
 import { AuthProvider } from "./Contexts/AuthContext";
 import AuthCallback from "./Pages/AuthCallback";
 import Homepage from "./Pages/Homepage";
@@ -20,17 +22,23 @@ export default function App() {
   return (
     <AuthProvider>
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/register" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<UserProfile />} />
-          <Route path="/auth/callback" element={<AuthCallback />} />
-          <Route path="*" element={<Navigate to="/" />} />
-          <Route path="/results" element={<Results />} />
-          <Route path="/publish" element={<PublishRide />} />
-        </Routes>
+        <div className="full-width-nav">
+          <Navbar />
+        </div>
+
+        <div className="app-content">
+          <SearchBar />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/register" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<UserProfile />} />
+            <Route path="/auth/callback" element={<AuthCallback />} />
+            <Route path="*" element={<Navigate to="/" />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/publish" element={<PublishRide />} />
+          </Routes>
+        </div>
       </Router>
     </AuthProvider>
   );
