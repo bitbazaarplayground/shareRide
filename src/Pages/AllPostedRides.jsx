e18475cb04947d0af4bc4d9457b177f45b2b942b
Nico2-NT <nicolastraver93@gmail.com>
2025-06-26 10:37:21 +0100

    Needs fixing message display in message component in navbar

--- src/Pages/AllPostedRides.jsx
+++ src/Pages/AllPostedRides.jsx
@@ -73,11 +73,17 @@ export default function AllPostedRides() {
               </div>
               {ride.profiles && (
                 <div className="poster-info">
-                  <img
-                    src={ride.profiles.avatar_url || "/default-avatar.png"}
-                    alt={`${ride.profiles.nickname}'s avatar`}
-                    className="poster-avatar"
-                  />
+                  {ride.profiles.avatar_url ? (
+                    <img
+                      src={ride.profiles.avatar_url}
+                      alt={`${ride.profiles.nickname}'s avatar`}
+                      className="poster-avatar"
+                    />
+                  ) : (
+                    <div className="poster-avatar initial-avatar">
+                      {ride.profiles.nickname?.charAt(0).toUpperCase() || "?"}
+                    </div>
+                  )}
                   <Link
                     to={`/profile/${ride.profiles.id}`}
                     className="poster-nickname"
@@ -86,6 +92,7 @@ export default function AllPostedRides() {
                   </Link>
                 </div>
               )}
+
               {ride.notes && (
                 <p className="ride-notes">
                   <em>{ride.notes}</em>
