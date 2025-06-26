8202e5a656893b1ec1d5cd5d0a374aa9e577df6b
Nico2-NT <nicolastraver93@gmail.com>
2025-06-26 10:37:21 +0100

    Needs fixing message display in message component in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import PublishRide from "./Components/PublishRide";
 import SearchBar from "./Components/SearchBar";
 import "./Components/Styles/Navbar.css";
 import { AuthProvider } from "./Contexts/AuthContext";
-import Chat from "./Messages/Chat";
 import ChatRoom from "./Messages/ChatRoom";
+import MessagesPage from "./Messages/MessagesPage";
 import AdminDashboard from "./Pages/AdminDashboard";
 import AllPostedRides from "./Pages/AllPostedRides";
 import AuthCallback from "./Pages/AuthCallback";
@@ -80,8 +80,8 @@ export default function App() {
           <Route path="/individual-ride/:id" element={<IndividualRide />} />
           <Route path="/Termsofuse" element={<TermsofUse />} />
           {/* Messaging */}
+          <Route path="/messages" element={<MessagesPage />} />
           <Route path="/chat/:partnerId" element={<ChatRoom />} />
-          <Route path="/chat/:userId" element={<Chat />} />
         </Routes>
       </div>
     </AuthProvider>
