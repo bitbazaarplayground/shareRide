4d40041d85f9fdc3967e832c4560403c4cf25402
Nico2-NT <nicolastraver93@gmail.com>
2025-07-08 15:15:31 +0100

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/shareRide/",
+  base: "/",
   plugins: [react()],
   server: {
     host: true,
