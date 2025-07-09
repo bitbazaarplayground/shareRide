7e514f1382141501ab77073de8ef3f62a4b9a009
Nico2-NT <nicolastraver93@gmail.com>
2025-07-09 13:10:36 +0100

    update Recovery password

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,34 @@
 import react from "@vitejs/plugin-react";
+import fs from "fs";
 import { defineConfig } from "vite";
+
+// Plugin to copy _redirects from public/ to dist/
+function copyRedirects() {
+  return {
+    name: "copy-redirects",
+    closeBundle() {
+      fs.copyFileSync("public/_redirects", "dist/_redirects");
+    },
+  };
+}
 
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [react(), copyRedirects()],
   server: {
     host: true,
     port: process.env.PORT || 5173,
   },
 });
+
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   base: "/",
+//   plugins: [react()],
+//   server: {
+//     host: true,
+//     port: process.env.PORT || 5173,
+//   },
+// });
